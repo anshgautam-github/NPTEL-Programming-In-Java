f761135cc2109283e1d2fefed311187db7a8ea41
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-04 22:24:06 +0530

    Create 03- Programming Assignment 3.java

---
+++ 01- Week 01/03- Programming Assignment 3.java
@@ -0,0 +1,9 @@
+// Write a Java program to print the multiplication table of a given number up to 5
+
+
+// Print the multiplication table of number up to 5
+
+for(int i=1;i<=number;i++){
+	System.out.println(number+" x "+i+" = "+i*number);
+	
+}

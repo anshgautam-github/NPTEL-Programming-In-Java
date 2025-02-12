8ef8bed2576fe73194099105c55ef51bca60ab76
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-12 22:06:54 +0530

    Create 04- Programming Assignment 4.java

---
+++ 03- Week 03/04- Programming Assignment 4.java
@@ -0,0 +1,22 @@
+// ================================
+    // TODO: Implement the Student class with private fields (name, age),
+    //       a constructor, and getter/setter methods.
+
+Student(String name, int age){
+	this.name=name;
+  	this.age=age;
+}
+
+public String getName(){
+	return name;
+}
+
+public int getAge(){
+	return age;
+}
+  public void setName(String name) {
+        this.name = name;
+    }
+  public void setAge(int age) {
+        this.age = age;
+    }

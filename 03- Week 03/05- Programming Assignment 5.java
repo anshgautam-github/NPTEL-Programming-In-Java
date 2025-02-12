82aabee4aa6052136470a0965e9ceed1402b4c00
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-12 22:07:28 +0530

    Create 05- Programming Assignment 5.java

---
+++ 03- Week 03/05- Programming Assignment 5.java
@@ -0,0 +1,4 @@
+//Call the method sum() to find the sum of two numbers and store in result1
+int result1=st.sum(n1,n2);
+//Call the method multiply() to find the product of two numbers and store in result2
+int result2=QuestionScope.multiply(n1,n2);

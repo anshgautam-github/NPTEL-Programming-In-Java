df2740c79a2473e8ca90ea8c383de59bb25a3fcc
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-12 22:04:23 +0530

    Create 02- Programming Assignment 2.java

---
+++ 03- Week 03/02- Programming Assignment 2.java
@@ -0,0 +1,21 @@
+//Create subclass cls2 of cls1 which perform task of multiplication and sum of squares of passing two parameters.
+
+class cls2 extends cls1 {
+		
+  void add(int p,int q){
+
+    System.out.println(p + q);
+  }
+  void mul(int p,int q){
+
+    System.out.println(p * q);
+  }
+  void task(int p,int q){
+
+    System.out.println((p*p) + (q*q));
+  }
+  
+  
+  
+  
+}

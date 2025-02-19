3d28f6ee5147657a34e636633e2d1bfaf606d9d0
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-19 15:45:04 +0530

    Create 01-Programming Assignment 1.java

---
+++ 04- Week 04/01-Programming Assignment 1.java
@@ -0,0 +1,5 @@
+
+//HINT1:  Import of pre-defined package java.util and class Scanner
+//HINT2:  Import of pre-defined package java.lang and class System and all of its member
+import java.util.Scanner; // HINT1: Import of pre-defined package java.util and class Scanner
+import static java.lang.System.out;

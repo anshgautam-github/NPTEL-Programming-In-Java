ebb5cc11c3c612369a24dcbf9bd2f14372d70fa1
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-02-20 20:36:03 +0530

    Update 05- Programming Assignment 5.java

--- 04- Week 04/05- Programming Assignment 5.java
+++ 04- Week 04/05- Programming Assignment 5.java
@@ -1,17 +1,11 @@
-//the code in this cell is sample code --please edit this to perform as asked in the question
-
-
-// Class ShapeG is created which implements ShapeY
-class ShapeG  implements ShapeY {
- public String base = "This is Shape3";
-      public void display1() {
-        System.out.println("Circle: " + ShapeX.base); // Prints "This is Shape1"
-        System.out.println("Circle: " + ShapeY.base); // Prints "This is Shape2"
+class ShapeG implements ShapeY  {
+ @Override
+    public void display1() {
+        System.out.println("Circle: " + ShapeX.base); // Accessing ShapeX.base explicitly
     }
 
     // Overriding method in ShapeY interface
+    @Override
     public void display2() {
-        System.out.println("Circle: " + this.base); // Prints "This is Shape3"
-        System.out.println("Circle: " + this.base); // Prints "This is Shape3"
-    }
-}
+        System.out.println("Circle: " + ShapeY.base); // Accessing ShapeY.base explicitly
+    }}
